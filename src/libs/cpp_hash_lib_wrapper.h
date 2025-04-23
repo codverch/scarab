1fc56b419fd448a0527ffa08b98a15a1dbab6ae0
Minya Rancic <msrancic@andrew.cmu.edu>
2025-04-23 10:52:24 -0400

    fixed oom error

--- src/libs/cpp_hash_lib_wrapper.h
+++ src/libs/cpp_hash_lib_wrapper.h
@@ -11,6 +11,7 @@ extern "C" {
   //class cpp_hash_lib_wrapper {
 
   Inst_Info * cpp_hash_table_access_create(int core, uint64_t addr, uint64_t lsb_bytes, uint64_t msb_bytes, uint8_t op_idx, unsigned char *new_entry);
+  void cpp_delete_info(Inst_Info* inst);
 
 #ifdef __cplusplus
 }
